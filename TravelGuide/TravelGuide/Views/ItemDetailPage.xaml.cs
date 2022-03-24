d737ba11bfd6841193591e21eeb246930ff73503
k-mitkov <krasi0m0@gmail.com>
2022-03-24 10:47:05 +0200

    Add project files.

---
+++ TravelGuide/TravelGuide/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TravelGuide.ViewModels;
+using Xamarin.Forms;
+
+namespace TravelGuide.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TravelGuide/TravelGuide/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TravelGuide.Models;
+using TravelGuide.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelGuide.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
